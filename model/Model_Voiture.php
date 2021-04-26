0af7e83f4f4e7abbccab6dd51d095608470e0c35
Lyes Bennour <lyessbennour@gmail.com>
2021-04-26 01:20:33 -0400

    finalisation recharche plusieurs criteres plus validaton des donnees entrees

--- model/Model_Voiture.php
+++ model/Model_Voiture.php
@@ -31,54 +31,78 @@
 
 		public function obtenirRechercheVoiture($idModele, $idFabricant, $anneeMin, $anneeMax, $prixMin, $prixMax) {
 			try {
-				if($idModele == "undefined"){
-					$stmt = $this->connexion->query("SELECT * from voiture 
-													INNER JOIN groupemotopropulseur on groupemotopropulseur.idGroupemotopropulseur = voiture.idGroupemotopropulseur 
-													INNER JOIN chassis on chassis.idChassis = voiture.idChassis 
-													INNER JOIN statut on statut.idStatut = voiture.idStatut 
-													INNER JOIN modele on modele.idModele = voiture.idModele 
-													INNER JOIN marque on marque.idMarque = modele.idMarque 
-													INNER JOIN fabricant on fabricant.idFabricant = marque.idFabricant 
-													INNER JOIN listeimage on listeimage.idVoiture = voiture.idVoiture 
-													WHERE listeimage.idVoiture = voiture.idVoiture AND listeimage.ordre = 1 
-													AND marque.idFabricant = $idFabricant AND voiture.annee BETWEEN $anneeMin AND $anneeMax
-													AND voiture.prixVente BETWEEN $prixMin AND $prixMax 
-													order by voiture.dateArrivee");
-					/* $stmt->bindParam(":mod", $idModele);
-					$stmt->bindParam(":fab", $idFabricant);
-					$stmt->bindParam(":anmin", $anneeMin);
-					$stmt->bindParam(":anmax", $anneeMax);
-					$stmt->bindParam(":primin", $prixMin);
-					$stmt->bindParam(":primax", $prixMax); */
+				if(isset($idModele) && isset($idFabricant) && isset($anneeMin) && isset($anneeMax) && isset($prixMin) && isset($prixMax)){
+					if($idModele == "undefined" && !$idFabricant == "undefined"){
+						$stmt = $this->connexion->query("SELECT * from voiture 
+														INNER JOIN groupemotopropulseur on groupemotopropulseur.idGroupemotopropulseur = voiture.idGroupemotopropulseur 
+														INNER JOIN chassis on chassis.idChassis = voiture.idChassis 
+														INNER JOIN statut on statut.idStatut = voiture.idStatut 
+														INNER JOIN modele on modele.idModele = voiture.idModele 
+														INNER JOIN marque on marque.idMarque = modele.idMarque 
+														INNER JOIN fabricant on fabricant.idFabricant = marque.idFabricant 
+														INNER JOIN listeimage on listeimage.idVoiture = voiture.idVoiture 
+														WHERE listeimage.idVoiture = voiture.idVoiture AND listeimage.ordre = 1 
+														AND marque.idFabricant = $idFabricant AND voiture.annee BETWEEN $anneeMin AND $anneeMax
+														AND voiture.prixVente BETWEEN $prixMin AND $prixMax 
+														order by voiture.dateArrivee");
+						/* $stmt->bindParam(":mod", $idModele);
+						$stmt->bindParam(":fab", $idFabricant);
+						$stmt->bindParam(":anmin", $anneeMin);
+						$stmt->bindParam(":anmax", $anneeMax);
+						$stmt->bindParam(":primin", $prixMin);
+						$stmt->bindParam(":primax", $prixMax); */
 
-					$stmt->execute();
-					return $stmt->fetchAll();
+						$stmt->execute();
+						return $stmt->fetchAll();
 
-				}else{
-					$stmt = $this->connexion->query("SELECT * from voiture
-													INNER JOIN groupemotopropulseur on groupemotopropulseur.idGroupemotopropulseur = voiture.idGroupemotopropulseur
-													INNER JOIN chassis on chassis.idChassis = voiture.idChassis
-													INNER JOIN statut on statut.idStatut = voiture.idStatut
-													INNER JOIN modele on modele.idModele = voiture.idModele
-													INNER JOIN marque on marque.idMarque = modele.idMarque
-													INNER JOIN fabricant on fabricant.idFabricant = marque.idFabricant
-													INNER JOIN listeimage on listeimage.idVoiture = voiture.idVoiture
-													WHERE listeimage.idVoiture = voiture.idVoiture AND listeimage.ordre = 1 
-													AND voiture.idModele = $idModele AND marque.idFabricant = $idFabricant AND voiture.annee BETWEEN $anneeMin AND $anneeMax
-													AND voiture.prixVente BETWEEN $prixMin AND $prixMax
-													order by voiture.dateArrivee");
-					/* $stmt->bindParam(":mod", $idModele);
-					$stmt->bindParam(":fab", $idFabricant);
-					$stmt->bindParam(":anmin", $anneeMin);
-					$stmt->bindParam(":anmax", $anneeMax);
-					$stmt->bindParam(":primin", $prixMin);
-					$stmt->bindParam(":primax", $prixMax); */
+					}else if($idModele == "undefined" && $idFabricant == "undefined"){
+						$stmt = $this->connexion->query("SELECT * from voiture
+														INNER JOIN groupemotopropulseur on groupemotopropulseur.idGroupemotopropulseur = voiture.idGroupemotopropulseur
+														INNER JOIN chassis on chassis.idChassis = voiture.idChassis
+														INNER JOIN statut on statut.idStatut = voiture.idStatut
+														INNER JOIN modele on modele.idModele = voiture.idModele
+														INNER JOIN marque on marque.idMarque = modele.idMarque
+														INNER JOIN fabricant on fabricant.idFabricant = marque.idFabricant
+														INNER JOIN listeimage on listeimage.idVoiture = voiture.idVoiture
+														WHERE listeimage.idVoiture = voiture.idVoiture AND listeimage.ordre = 1 
+														AND voiture.annee BETWEEN $anneeMin AND $anneeMax
+														AND voiture.prixVente BETWEEN $prixMin AND $prixMax
+														order by voiture.dateArrivee");
+						/* $stmt->bindParam(":mod", $idModele);
+						$stmt->bindParam(":fab", $idFabricant);
+						$stmt->bindParam(":anmin", $anneeMin);
+						$stmt->bindParam(":anmax", $anneeMax);
+						$stmt->bindParam(":primin", $prixMin);
+						$stmt->bindParam(":primax", $prixMax); */
+						$stmt->execute();
+						return $stmt->fetchAll();
 
-					$stmt->execute();
-					return $stmt->fetchAll();
+					}
+					else{
+						$stmt = $this->connexion->query("SELECT * from voiture
+														INNER JOIN groupemotopropulseur on groupemotopropulseur.idGroupemotopropulseur = voiture.idGroupemotopropulseur
+														INNER JOIN chassis on chassis.idChassis = voiture.idChassis
+														INNER JOIN statut on statut.idStatut = voiture.idStatut
+														INNER JOIN modele on modele.idModele = voiture.idModele
+														INNER JOIN marque on marque.idMarque = modele.idMarque
+														INNER JOIN fabricant on fabricant.idFabricant = marque.idFabricant
+														INNER JOIN listeimage on listeimage.idVoiture = voiture.idVoiture
+														WHERE listeimage.idVoiture = voiture.idVoiture AND listeimage.ordre = 1 
+														AND voiture.idModele = $idModele AND marque.idFabricant = $idFabricant AND voiture.annee BETWEEN $anneeMin AND $anneeMax
+														AND voiture.prixVente BETWEEN $prixMin AND $prixMax
+														order by voiture.dateArrivee");
+						/* $stmt->bindParam(":mod", $idModele);
+						$stmt->bindParam(":fab", $idFabricant);
+						$stmt->bindParam(":anmin", $anneeMin);
+						$stmt->bindParam(":anmax", $anneeMax);
+						$stmt->bindParam(":primin", $prixMin);
+						$stmt->bindParam(":primax", $prixMax); */
 
+						$stmt->execute();
+						return $stmt->fetchAll();
+
+					}
 				}
-
 			}
 			catch(Exception $exc) {
 				var_dump($exc->getMessage());
--- view/FiltrerPlusieursCriteres.php
+++ view/FiltrerPlusieursCriteres.php
@@ -1,8 +1,8 @@
-<section class="FiltrerPlusieursCriteres" data-component="FiltrerPlusieursCriteres">
+<section class="FiltrerPlusieursCriteres" data-component="FiltrerPlusieursCriteres" data-js-component="Form">
 
 	<!-- plusieurs criteres -->
 	<form>	
-		<div>
+		<div class="input-wrapper" data-js-input-wrapper>
 			<label>Modele :</label><br>
 			<select name="modele" data-js-modele size=1>
 			<option value=""></option>
@@ -17,8 +17,9 @@
 		}
 		?>
 			</select>
+			<small class="error-message" data-js-error-msg></small>
 		</div>
-		<div>
+		<div class="input-wrapper" data-js-input-wrapper>
 			<label>Fabricant :</label><br>
 			<select name="fabricant" data-js-fabricant size=1>
 			<option value=""></option>
@@ -33,16 +34,19 @@
 		}
 		?>
 			</select>
+			<small class="error-message" data-js-error-msg></small>
 		</div>
-		<div>
+		<div class="input-wrapper" data-js-input-wrapper>
 			<label>Annee :</label><br>
-			<input name="annee_min" data-component="anneeMin" placeholder="1900" maxlength="4" min="1900" max=<?=date('Y')?>>
-			<input name="annee_max" data-component="anneeMax" placeholder="<?=date('Y')?>" maxlength="4" min="1900" max=<?=date('Y')?>><br>
+			<input name="annee_min" data-js-annee data-component="anneeMin" placeholder="1900" maxlength="4" min="1900" max=<?=date('Y')?>>
+			<input name="annee_max" data-js-annee data-component="anneeMax" placeholder="<?=date('Y')?>" maxlength="4" min="1900" max=<?=date('Y')?>><br>
+			<small class="error-message" data-js-error-msg></small>
 		</div>
-		<div>
+		<div class="input-wrapper" data-js-input-wrapper>
 			<label>Prix :</label><br>
-			<input type="number" data-component="prixMin" name="prix_min" placeholder="De" maxlength="5" mainlength="0">
-			<input type="number" data-component="prixMax" name="prix_max" placeholder="À" maxlength="6" mainlength="0">
+			<input type="number" data-js-prix data-component="prixMin" name="prix_min" placeholder="De" maxlength="5" mainlength="0">
+			<input type="number" data-js-prix data-component="prixMax" name="prix_max" placeholder="À" maxlength="6" mainlength="0"><br>
+			<small class="error-message" data-js-error-msg></small>
 		</div>
 		<button class="disabled" data-js-btn>Recherche</button>
 	</form>
