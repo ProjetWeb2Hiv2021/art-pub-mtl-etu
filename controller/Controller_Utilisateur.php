e7fb555fde278501aa66304cbcb40c554e5b5535
David Kavanaght <kavanaght.david@courrier.uqam.ca>
2021-04-25 22:30:31 -0400

    Ajout formulaire connexion et authentification sans le hash

---
+++ controller/Controller_Utilisateur.php
@@ -0,0 +1,67 @@
+<?php
+	class Controller_Utilisateur extends BaseController {
+	
+		//la fonction qui sera appelée par le routeur
+		public function traite(array $params) {
+			// Initialiser la vue et la session
+            $vue = "";
+            session_start();
+
+			$this->showView("Head");
+			$this->showView("Header");
+			
+			if (isset($params["action"])) {
+				// Modèle et vue vides par défaut
+				$data = array();
+				
+				// Switch en fonction de l'action qui nous est envoyée
+				// Ce switch détermine la vue $vue et obtient le modèle $data
+				switch($params["action"]) {
+					 /* Mettre des case selon les paramètres  
+                    ne pas oublier le "default:"*/
+                    case "connexion":
+                        $vue = "ConnexionSWT";  						
+						$this->showView($vue);
+                        break;
+					case "authentifier":
+                        //var_dump($_REQUEST);
+						// Vérifier qu'on a bien un utilisateur et un mot de passe
+						if(isset($_REQUEST["nomUtilisateur"], $_REQUEST["motPasse"]))
+						{
+							// obtenir le modele
+							$modeleUtilisateur = new Model_Utilisateur();
+							// Vérifier si le mot de passe encrypte et celui fourni sont bien égaux
+							$authentifie = $modeleUtilisateur->authentification($_REQUEST["nomUtilisateur"],$_REQUEST["motPasse"]);
+							// Si c'est le cas 
+							if($authentifie)
+							{
+								$modeleTypeUtilisateur = new Model_TypeUtilisateur();
+								// Définir le champ usager de la variable session comme l'usager courant
+								$_SESSION["nomUtilisateur"] = $_REQUEST["nomUtilisateur"];
+								//var_dump($modeleTypeUtilisateur->obtenirTypeUtilisateur($_REQUEST["nomUtilisateur"]));
+								$_SESSION["typeUtilisateur"] = $modeleTypeUtilisateur->obtenirTypeUtilisateur($_REQUEST["nomUtilisateur"])["typeUtilisateur"];
+								$vue = "Utilisateur";
+								// Afficher la vue compléter les champs
+								$this->showView($vue, $data);
+								//var_dump($_SESSION);
+								
+							}else
+							{                        
+								// Sinon ajouter le message expliquant l'erreur
+								$data["message"] = "Combinaison 'nom d'usager' / 'mot de passe' invalide.";                        
+								// Retourner au formulaire de connexion
+								$vue = "ConnexionSWT";  						
+								$this->showView($vue, $data);
+							}
+
+						}
+                        break;
+				}			
+			} else {
+				
+				
+			}
+			$this->showView("Footer");
+		}
+	}
+?>
--- view/Header.php
+++ view/Header.php
@@ -1,3 +1,16 @@
-<header class="header" data-component="Header">
-	<h1>carsareus</h1>
+<header class="header row distribue" data-component="Header">
+	<h1>Cars are Us</h1>
+	<div class="connexion row centreV">
+		
+		<?php if(isset($_SESSION["typeUtilisateur"])&& $_SESSION["typeUtilisateur"]=="Administrateur") echo "<span>adm&nbsp;</span>"; ?>
+		<?php if(isset($_SESSION["typeUtilisateur"])&& $_SESSION["typeUtilisateur"]=="Client") echo "<span>client&nbsp;</span>"; ?>
+		<?php if(isset($_SESSION["typeUtilisateur"])&& $_SESSION["typeUtilisateur"]=="Employe") echo "<span>emp&nbsp;</span>"; ?>
+		<?php if(!isset($_SESSION["nomUtilisateur"])) echo "<span>Connexion</span>"; ?>
+      	
+      	<span>
+		  	<a href="index.php?Utilisateur&action=connexion">
+				<img src="./assets/images/login.svg" alt="Connexion" />
+			</a>
+		</span>
+    </div>
 </header>
