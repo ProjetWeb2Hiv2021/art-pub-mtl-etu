89cc776e318b5aba08c60777fe38b8c216c21cb4
Lyes Bennour <lyessbennour@gmail.com>
2021-04-30 00:45:06 -0400

    integration vue vedette modification liste voiture plus filtrer et trier

--- model/Model_Voiture.php
+++ model/Model_Voiture.php
@@ -33,7 +33,7 @@
 			try {
 				if(isset($idModele) && isset($idFabricant) && isset($anneeMin) && isset($anneeMax) && isset($prixMin) && isset($prixMax)){
 					if($idModele == "NaN" && $idFabricant !== "NaN"){
-						$stmt = $this->connexion->query("SELECT * from voiture 
+						$stmt = $this->connexion->prepare("SELECT * from voiture 
 														INNER JOIN groupemotopropulseur on groupemotopropulseur.idGroupemotopropulseur = voiture.idGroupemotopropulseur 
 														INNER JOIN chassis on chassis.idChassis = voiture.idChassis 
 														INNER JOIN statut on statut.idStatut = voiture.idStatut 
@@ -42,21 +42,20 @@
 														INNER JOIN fabricant on fabricant.idFabricant = marque.idFabricant 
 														INNER JOIN listeimage on listeimage.idVoiture = voiture.idVoiture 
 														WHERE listeimage.idVoiture = voiture.idVoiture AND listeimage.ordre = 1 
-														AND marque.idFabricant = $idFabricant AND voiture.annee BETWEEN $anneeMin AND $anneeMax
-														AND voiture.prixVente BETWEEN $prixMin AND $prixMax 
-														order by voiture.dateArrivee");
-						/* $stmt->bindParam(":mod", $idModele);
+														AND marque.idFabricant = :fab AND voiture.annee BETWEEN :anmin AND :anmax
+														AND voiture.prixVente BETWEEN :primin AND :primax
+														order by voiture.dateArrivee");			
 						$stmt->bindParam(":fab", $idFabricant);
 						$stmt->bindParam(":anmin", $anneeMin);
 						$stmt->bindParam(":anmax", $anneeMax);
 						$stmt->bindParam(":primin", $prixMin);
-						$stmt->bindParam(":primax", $prixMax); */
+						$stmt->bindParam(":primax", $prixMax);
 
 						$stmt->execute();
 						return $stmt->fetchAll();
 
 					}else if($idModele == "NaN" && $idFabricant == "NaN"){
-						$stmt = $this->connexion->query("SELECT * from voiture
+						$stmt = $this->connexion->prepare("SELECT * from voiture
 														INNER JOIN groupemotopropulseur on groupemotopropulseur.idGroupemotopropulseur = voiture.idGroupemotopropulseur
 														INNER JOIN chassis on chassis.idChassis = voiture.idChassis
 														INNER JOIN statut on statut.idStatut = voiture.idStatut
@@ -65,21 +64,20 @@
 														INNER JOIN fabricant on fabricant.idFabricant = marque.idFabricant
 														INNER JOIN listeimage on listeimage.idVoiture = voiture.idVoiture
 														WHERE listeimage.idVoiture = voiture.idVoiture AND listeimage.ordre = 1 
-														AND voiture.annee BETWEEN $anneeMin AND $anneeMax
-														AND voiture.prixVente BETWEEN $prixMin AND $prixMax
+														AND voiture.annee BETWEEN :anmin AND :anmax
+														AND voiture.prixVente BETWEEN :primin AND :primax
 														order by voiture.dateArrivee");
-						/* $stmt->bindParam(":mod", $idModele);
-						$stmt->bindParam(":fab", $idFabricant);
+						
 						$stmt->bindParam(":anmin", $anneeMin);
 						$stmt->bindParam(":anmax", $anneeMax);
 						$stmt->bindParam(":primin", $prixMin);
-						$stmt->bindParam(":primax", $prixMax); */
+						$stmt->bindParam(":primax", $prixMax);
 						$stmt->execute();
 						return $stmt->fetchAll();
 
 					}
 					else{
-						$stmt = $this->connexion->query("SELECT * from voiture
+						$stmt = $this->connexion->prepare("SELECT * from voiture
 														INNER JOIN groupemotopropulseur on groupemotopropulseur.idGroupemotopropulseur = voiture.idGroupemotopropulseur
 														INNER JOIN chassis on chassis.idChassis = voiture.idChassis
 														INNER JOIN statut on statut.idStatut = voiture.idStatut
@@ -88,15 +86,15 @@
 														INNER JOIN fabricant on fabricant.idFabricant = marque.idFabricant
 														INNER JOIN listeimage on listeimage.idVoiture = voiture.idVoiture
 														WHERE listeimage.idVoiture = voiture.idVoiture AND listeimage.ordre = 1 
-														AND voiture.idModele = $idModele AND marque.idFabricant = $idFabricant AND voiture.annee BETWEEN $anneeMin AND $anneeMax
-														AND voiture.prixVente BETWEEN $prixMin AND $prixMax
+														AND voiture.idModele = :mod AND marque.idFabricant = :fab AND voiture.annee BETWEEN :anmin AND :anmax
+														AND voiture.prixVente BETWEEN :primin AND :primax
 														order by voiture.dateArrivee");
-						/* $stmt->bindParam(":mod", $idModele);
+						$stmt->bindParam(":mod", $idModele);
 						$stmt->bindParam(":fab", $idFabricant);
 						$stmt->bindParam(":anmin", $anneeMin);
 						$stmt->bindParam(":anmax", $anneeMax);
 						$stmt->bindParam(":primin", $prixMin);
-						$stmt->bindParam(":primax", $prixMax); */
+						$stmt->bindParam(":primax", $prixMax);
 
 						$stmt->execute();
 						return $stmt->fetchAll();
@@ -137,6 +135,28 @@
 			catch(Exception $exc) {
 				var_dump($exc->getMessage());
 
+				return 0;
+			}
+		}
+		public function obtenirListeVoitureVedette() {
+			
+			try {
+				$stmt = $this->connexion->query("SELECT * from voiture
+												INNER JOIN groupeMotopropulseur on groupeMotopropulseur.idGroupemotopropulseur = voiture.idGroupemotopropulseur
+												INNER JOIN chassis on chassis.idChassis = voiture.idChassis
+												INNER JOIN statut on statut.idStatut = voiture.idStatut
+												INNER JOIN modele on modele.idModele = voiture.idModele
+												INNER JOIN marque on marque.idMarque = modele.idMarque
+												INNER JOIN fabricant on fabricant.idFabricant = marque.idFabricant
+												INNER JOIN listeImage on listeImage.idVoiture = voiture.idVoiture
+												WHERE listeImage.idVoiture = voiture.idVoiture AND listeImage.ordre = 1 AND voiture.vedette = 1
+												order by voiture.dateArrivee");
+
+				$stmt->execute();
+				return $stmt->fetchAll();
+
+			}
+			catch(Exception $exc) {
 				return 0;
 			}
 		}
