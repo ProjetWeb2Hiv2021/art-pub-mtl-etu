58a2c313b6a02c715548534239ef3557d4a89a95
David Kavanaght <kavanaght.david@courrier.uqam.ca>
2021-05-01 13:55:46 -0400

    arranger carousel

--- view/VoitureVedette.php
+++ view/VoitureVedette.php
@@ -1,50 +1,50 @@
 <section data-component="VoitureVedette" class="swiper-container">
  <!-- juste un prototype --> 
- <div class="swiper-wrapper">
-
-    <?php
-    
-    if($data["voitureVedette"]){
-        foreach ($data["voitureVedette"] as $voiture) {
-            
-    ?>
-            <article class="voiture_liste__voiture swiper-slide" 
-                data-js-voiture
-                data-js-voiture-id="<?= $voiture["idVoiture"] ?>" 
-                data-js-voiture-vin="<?= $voiture["vin"] ?>"
-                data-js-voiture-prixVente="<?= $voiture["prixVente"] ?>"
-                data-js-voiture-km="<?= $voiture["km"] ?>" 
-                data-js-voiture-annee="<?= $voiture["annee"] ?>" 
-                data-js-voiture-modele="<?= $voiture["idModele"] ?>" 
-                data-js-voiture-modele="<?= $voiture["couleur"] ?>" 
-                data-js-voiture-prix="<?= $voiture["prixPaye"] ?>" 
-                data-js-voiture-groupeMotopropulseur="<?= $voiture["groupeMotopropulseur"] ?>" 
-                data-js-voiture-modele="<?= $voiture["modele"] ?>"
-                data-js-voiture-marque="<?= $voiture["marque"] ?>"
-                data-js-voiture-fabricant="<?= $voiture["fabricant"] ?>"
-                data-js-voiture-statut="<?= $voiture["statut"] ?>"
-                data-component="Voiture"
-            >
-    
-                <div class="voiture_liste__image-wrapper">
-                    <img src="<?= $voiture["cheminFichier"] ?>" alt="" class="voiture_liste__image">
-                </div>        
-            </article>
-            <hr>
-    <?php
-            
-        }
-
-        
-            }else{
-        ?>  
-               <p>pas de voitures en vedette</p>
-
+ 
+    <div class="carousel">
         <?php
-        }
-    ?>
-    <!-- Fin prototype -->
+        
+        if($data["voitureVedette"]){
+            foreach ($data["voitureVedette"] as $voiture) {
+                
+            ?>  <div>
+                    <div 
+                        data-js-voiture
+                        data-js-voiture-id="<?= $voiture["idVoiture"] ?>" 
+                        data-js-voiture-vin="<?= $voiture["vin"] ?>"
+                        data-js-voiture-prixVente="<?= $voiture["prixVente"] ?>"
+                        data-js-voiture-km="<?= $voiture["km"] ?>" 
+                        data-js-voiture-annee="<?= $voiture["annee"] ?>" 
+                        data-js-voiture-modele="<?= $voiture["idModele"] ?>" 
+                        data-js-voiture-modele="<?= $voiture["couleur"] ?>" 
+                        data-js-voiture-prix="<?= $voiture["prixPaye"] ?>" 
+                        data-js-voiture-groupeMotopropulseur="<?= $voiture["groupeMotopropulseur"] ?>" 
+                        data-js-voiture-modele="<?= $voiture["modele"] ?>"
+                        data-js-voiture-marque="<?= $voiture["marque"] ?>"
+                        data-js-voiture-fabricant="<?= $voiture["fabricant"] ?>"
+                        data-js-voiture-statut="<?= $voiture["statut"] ?>"
+                        data-component="Voiture" class="box"
+                    >
+            
+                        
+                            <img src="<?= $voiture["cheminFichier"] ?>" alt="" class="voiture_liste__image">
+                        
+                    </div>
+                </div>
+            <?php
+                
+            }
+
+            
+        }else{
+            ?>  
+                <p>pas de voitures en vedette</p>
+
+            <?php
+         }
+        ?>
+        <!-- Fin prototype -->
+        
     </div>
-</div>
 
 </section>
