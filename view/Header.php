f2f957de37fbd23e7b48738c79aa44ca7130a07a
Ricardo Basten <rbastenc@gmail.com>
2021-05-03 06:03:40 -0300

    ligne 4 du header.php en commentaire

--- view/Header.php
+++ view/Header.php
@@ -4,8 +4,6 @@
 <!--<img src="./assets/images/logo.svg"/>-->
 	<div class="connexion ligne centreV">
 
-
-		
 		<?php if(isset($_SESSION["typeUtilisateur"])&& $_SESSION["typeUtilisateur"]=="Administrateur") echo "<span>adm&nbsp;</span>"; ?>
 		<?php if(isset($_SESSION["typeUtilisateur"])&& $_SESSION["typeUtilisateur"]=="Client") echo "<span>client&nbsp;</span>"; ?>
 		<?php if(isset($_SESSION["typeUtilisateur"])&& $_SESSION["typeUtilisateur"]=="Employe") echo "<span>emp&nbsp;</span>";  ?>
