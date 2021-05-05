6587aa21a579213ca0c0ae45f09c152b455f053a
David Kavanaght <kavanaght.david@courrier.uqam.ca>
2021-05-05 10:07:08 -0400

    design page utilisateurSystemes

--- index.php
+++ index.php
@@ -3,15 +3,15 @@
 	/*
 	https://www.php.net/manual/fr/language.oop5.autoload.php
 	*/
-    /* il serait bien d'avoir la meme architecture pour toute l'equipe*/
-    /*/projetWeb2Equipe4Lebon/*/ 
-
-     define("RACINE", $_SERVER["DOCUMENT_ROOT"] . "/projet_web_2/projetWeb2Equipe4Lebon/");
-
-	define("RACINEWEB", "http://" . $_SERVER["SERVER_NAME"] . ":" . $_SERVER["SERVER_PORT"] . "/projet_web_2/projetWeb2Equipe4Lebon/"); 
     
-    /* define("RACINE", $_SERVER["DOCUMENT_ROOT"] . "/projet_web_2/projetWeb2Equipe4Lebon/");
-	define("RACINEWEB", "http://" . $_SERVER["SERVER_NAME"] . ":" . $_SERVER["SERVER_PORT"] . "/projet_web_2/projetWeb2Equipe4Lebon/"); */
+    define("RACINE", $_SERVER["DOCUMENT_ROOT"] . "/projet_web_2/projetWeb2Equipe4Lebon/");
+
+	define("RACINEWEB", "http://" . $_SERVER["SERVER_NAME"] . ":" . $_SERVER["SERVER_PORT"] . "/projet_web_2/projetWeb2Equipe4Lebon/");
+/*     define("RACINE", $_SERVER["DOCUMENT_ROOT"] . "/");
+
+	define("RACINEWEB", "http://" . $_SERVER["SERVER_NAME"] . ":" . $_SERVER["SERVER_PORT"] . "/");  */ 
+    
+
 	
 
 
