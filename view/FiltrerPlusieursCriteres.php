bdc3d7eae1484689e49bcc6643911cb604cf3504
David Kavanaght <kavanaght.david@courrier.uqam.ca>
2021-05-12 13:23:30 -0400

    aligner verticalement Filtrer Voiture plus corrections fichier langue

--- view/FiltrerPlusieursCriteres.php
+++ view/FiltrerPlusieursCriteres.php
@@ -1,11 +1,11 @@
 <section class="FiltrerPlusieursCriteres" data-component="FiltrerPlusieursCriteres">
 
 	<!-- plusieurs criteres -->
-	<form class="ligne">	
+	<form class="ligne centreV">	
 		<h3><?= TXT_FILTRER_VOITURES?> :</h3>
 
 		<div class="input-wrapper" data-js-input-wrapper>
-			<label><?= TXT_MARQUE?>  :</label><br>
+			<label><?= TXT_MARQUE?>  </label><br>
 			<select name="marque" data-js-marque size=1>
 			<option value="">Filtrer par marque</option>
 		<?php
@@ -23,7 +23,7 @@
 		</div>
 
 		<div class="input-wrapper" data-js-input-wrapper>
-			<label><?= TXT_MODELE?>  :</label><br>
+			<label><?= TXT_MODELE?>  </label><br>
 			<select name="modele" data-js-modele size=1>
 			<option value="">Filtrer par modèle</option>
 		<?php
@@ -41,13 +41,13 @@
 		</div>
 		
 		<div class="input-wrapper" data-js-input-wrapper>
-			<label><?= TXT_ANNEE?>  :</label><br>
+			<label><?= TXT_ANNEE?>  </label><br>
 			<input name="annee_min" data-js-annee data-component="anneeMin" placeholder="1900" maxlength="4" min="1900" max=<?=date('Y')?>>
 			<input name="annee_max" data-js-annee data-component="anneeMax" placeholder="<?=date('Y')?>" maxlength="4" min="1900" max=<?=date('Y')?>><br>
 			<small class="error-message" data-js-error-msg></small>
 		</div>
 		<div class="input-wrapper" data-js-input-wrapper>
-			<label><?= TXT_PRIX?> :</label><br>
+			<label><?= TXT_PRIX?> </label><br>
 			<input type="number" data-js-prix data-component="prixMin" name="prix_min" placeholder="De" maxlength="5" mainlength="0">
 			<input type="number" data-js-prix data-component="prixMax" name="prix_max" placeholder="À" maxlength="6" mainlength="0"><br>
 			<small class="error-message" data-js-error-msg></small>
